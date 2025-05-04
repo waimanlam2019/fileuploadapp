c5fa2f4b914ad5ace7a30435bb3b7119a29486e6
Wai Man Lam <lamwaiman1988@gmail.com>
2025-05-04 19:10:35 +0800

    updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ function App() {
   const auth = useAuth();
 
   const signOutRedirect = () => {
-    const clientId = "2dsvb0pp65uk4tksl6j1qi70s7";
-    const logoutUri = "http://localhost:5173/";
-    const cognitoDomain = "https://us-east-1x7nkctxsz.auth.us-east-1.amazoncognito.com";
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const logoutUri = import.meta.env.VITE_LOGOUT_URI;
+    const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
+
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
@@ -27,13 +28,14 @@ function App() {
   if (auth.isAuthenticated) {
     return (
       <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> User email: {auth.user?.profile.email} </pre>
+        {/*<pre> ID Token: {auth.user?.id_token} </pre>
         <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre> */}
+        
+        <button className='top-right' onClick={() => auth.removeUser()}>Sign out</button>
         <FileUpload accessToken={auth.user?.access_token} />
-        <button onClick={() => auth.removeUser()}>Sign out</button>
+        
       </div>
     );
   }
